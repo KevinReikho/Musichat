e66c7093de84b781246c1aae287fdef1b9cd2eed
KevinReikho <etudiant@LYON-iMac-45.local>
2017-02-07 11:54:27 +0100

    PDO update 2

--- messages.php
+++ messages.php
@@ -4,7 +4,7 @@
 // on se connecte à notre base de données
 try
 {
-    $bdd = new PDO('mysql:host=localhost;dbname=musicon', 'root', 'root');
+    $bdd = new PDO('mysql:host=localhost;dbname=musicon', 'musicon', 'musicon');
 }
 catch (Exception $e)
 {
