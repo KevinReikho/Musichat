663581cad500d8489e90f762271424b5e9e0a323
KevinReikho <etudiant@LYON-iMac-45.local>
2017-02-07 12:54:53 +0100

    print

--- traitement.php
+++ traitement.php
@@ -27,7 +27,7 @@ if(isset($_POST['submit'])){ // si on a envoyé des données avec le formulaire
       echo "error code : " ;
       echo $insertion->errorCode();
       echo "<br /> error info : ";
-      echo $insertion->errorInfo();
+      print_r ($insertion->errorInfo());
 
     }
     else{
