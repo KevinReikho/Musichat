56f7ae26b0ce0c55f3122de05395bd94a9eb648e
KevinReikho <etudiant@LYON-iMac-45.local>
2017-02-13 16:12:00 +0100

    AJAX print Have to optimize printed messages

--- messages.php
+++ messages.php
@@ -12,10 +12,21 @@ catch (Exception $e)
 }
 
 
-    // on récupère les messages ayant un id plus grand que celui donné
-    $requete = $bdd->prepare('SELECT * FROM message');
-    $messages = null;
-    $requete->execute();
+    if (isset($_GET['id'])){
+      $id = (int) $_GET['id'];
+      // on récupère les messages ayant un id plus grand que celui donné
+      $requete = $bdd->prepare('SELECT * FROM message WHERE id > :id ORDER BY id DESC');
+      $messages = null;
+      $requete->execute(array(
+        'id' => $id
+      ));
+    } else {
+      $requete = $bdd->prepare('SELECT * FROM message ');
+      $messages = null;
+      $requete->execute();
+
+    }
+
 
     while ($donnees = $requete->fetch()){
    $messages .= "<p id=\"" . $donnees['id_message'] . "\">" . $donnees['auteur'] . " dit : " . $donnees['message'] . "</p>";
--- script.js
+++ script.js
@@ -10,5 +10,41 @@ $('#envoi').click(function(e){
             type : "POST", // la requête est de type POST
             data : "auteur=" + auteur + "&message=" + message + "&submit=1"// et on envoie nos données
         });
+
+        $('#messages').append("<p>" + auteur + " dit : " + message + "</p>");
     }
 });
+
+function init() {
+  $.ajax({
+      url : "messages.php",
+      type : 'GET',
+      success : function(html){
+          $('#messages').prepend(html); // on veut ajouter les nouveaux messages au début du bloc #messages
+      }
+  });
+}
+
+init();
+
+function charger(){
+
+  var firstID = $('#messages p:last').attr('id');
+
+    setTimeout( function(){
+        // on lance une requête AJAX
+        $.ajax({
+            url : "messages.php?id=" + firstID,
+            type : 'GET',
+            success : function(html){
+                $('#messages').prepend(html); // on veut ajouter les nouveaux messages au début du bloc #messages
+            }
+        });
+
+        charger(); // on relance la fonction
+
+    }, 2000); // on exécute le chargement toutes les 5 secondes
+
+}
+
+charger();
