b44cd8d6ff66cd01659ce54d3c2705939e37c566
KevinReikho <etudiant@LYON-iMac-45.local>
2017-02-13 15:18:34 +0100

    null added
    added

--- traitement.php
+++ traitement.php
@@ -18,7 +18,7 @@ if(isset($_POST['submit'])){ // si on a envoyé des données avec le formulaire
         $message = $_POST['message'];
 
         // puis on entre les données en base de données :
-        $insertion = $bdd->prepare('INSERT INTO message VALUES("", :timestampvalue, :auteur, :message)');
+        $insertion = $bdd->prepare('INSERT INTO message VALUES(null, :timestampvalue, :auteur, :message)');
         $insertion->execute(array(
             'timestampvalue' => time(),
             'auteur' => $auteur,
